c981ef92f8b26f42a6afdf6f9c004b99a3fb4388
Berkin Kınay <103454964+developedbyven@users.noreply.github.com>
2022-10-03 22:42:17 +0300

    Add files via upload

---
+++ src/pages/Moreinfo.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Moreinfo = () => {
+  return (
+  
+ <div className='text'>
+   <div
+    class=
+    'flex h-screen w-full 2xl:h-screen 2xl:w-full bg-zinc-900 text-white items-center xl:flex xl:flex-col justify-center'>
+      <div 
+        class=
+        'flex xl:grid xl:grid-cols xl:items-center xl:justify-center h-[490px] xl:h-[990px] w-full xl:mt-[340px] lg:mt-[800px] bg-gray-800 md:h-[980px] sm:w-full'
+       > 
+        <div 
+         class=
+        'flex! w-2/3 xl:w1/3 h-[370px] bg-white mt-12 rounded-[70px] ml-2 xl:mt-24 lg:ml-24 sm:h-[700px] sm:w-64 sm:flex!'
+        >
+          <div    
+           class=
+           'grid grid-rows-3 gap-12 h-56 w-full items-center justify-center mt-[80px] xl:h-64 xl:mb-24 3xl:mx-8 sm:mx-1 sm:mt-36'
+           >
+           <div class='text-gray-700 font-semibold text-xl 2xl:text-lg mr-2 sm:mr-5 sm:flex sm:right-24' 
+            > 
+              I live in İzmir and I'm 23. 🙍‍♂️ Graduated from Computer Programming (Computer Science) and
+              I really love computer science, doing my job with learning new features about Software Development.
+            </div> 
+           <div class='mt-2 text-xl 2xl:text-lg xl:text-base text-gray-500 mr-2 font-bold sm:mt-36 sm:w-fit'
+            >
+              I like to try and use the futures with the new technologies. I'm
+              working hard for the best UI and UX experience. 🖥️
+           </div>
+           <div class='text-xl 2xl:text-lg xl:text-base text-gray-500 mr-5 font-bold sm:mr-24 sm:mt-72 sm:w-fit'
+            > 
+             Everything starts as an idea and successfully executing the process of doing it
+             all by writing code is just one of the best things I’ve ever done. 💫
+           </div>
+          </div>
+        </div>    
+       <div 
+        class=
+        'flex w-2/3 h-[440px] mt-4 rounded-2xl items-center justify-center lg:ml-36 sm:h-24 sm:w-fit'
+        >   gonna add content here coming soon :)
+         <div/>
+       </div>
+    </div>  
+  </div>
+</div>
+  )
+}
+
+export default Moreinfo;
