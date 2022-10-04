e34280923cc6e45f84a8f21d596170beb1c47f46
Berkin Kınay <103454964+developedbyven@users.noreply.github.com>
2022-10-04 21:21:54 +0300

    Update Moreinfo.js

--- src/pages/Moreinfo.js
+++ src/pages/Moreinfo.js
@@ -13,23 +13,23 @@ const Moreinfo = () => {
        > 
         <div 
          class=
-        'flex! w-2/3 xl:w1/3 h-[370px] bg-white mt-12 rounded-[70px] ml-2 xl:mt-24 lg:ml-24 sm:h-[700px] sm:w-64 sm:flex!'
+        'flex! w-2/3 xl:w1/3 h-[370px] mt-12 rounded-[70px] ml-2 xl:mt-24 lg:ml-24 sm:h-[700px] sm:w-64 sm:flex!'
         >
           <div    
            class=
            'grid grid-rows-3 gap-12 h-56 w-full items-center justify-center mt-[80px] xl:h-64 xl:mb-24 3xl:mx-8 sm:mx-1 sm:mt-36'
            >
-           <div class='text-gray-700 font-semibold text-xl 2xl:text-lg mr-2 sm:mr-5 sm:flex sm:right-24' 
+           <div class='text-gray-500 font-semibold text-xl 2xl:text-lg mr-2 sm:mr-5 sm:flex sm:right-24' 
             > 
               I live in İzmir and I'm 23. 🙍‍♂️ Graduated from Computer Programming (Computer Science) and
               I really love computer science, doing my job with learning new features about Software Development.
             </div> 
-           <div class='mt-2 text-xl 2xl:text-lg xl:text-base text-gray-500 mr-2 font-bold sm:mt-36 sm:w-fit'
+           <div class='mt-2 text-xl 2xl:text-lg xl:text-base text-gray-200 mr-14 font-semibold sm:mt-36 sm:w-fit'
             >
               I like to try and use the futures with the new technologies. I'm
               working hard for the best UI and UX experience. 🖥️
            </div>
-           <div class='text-xl 2xl:text-lg xl:text-base text-gray-500 mr-5 font-bold sm:mr-24 sm:mt-72 sm:w-fit'
+           <div class='text-xl 2xl:text-lg xl:text-base text-gray-200 mr-5 font-semibold sm:mr-24 sm:mt-72 sm:w-fit'
             > 
              Everything starts as an idea and successfully executing the process of doing it
              all by writing code is just one of the best things I’ve ever done. 💫
